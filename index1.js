baaf08ecada1541db902daf6010288f6cf5f2f6e
koog7 <k.ognev709@mail.com>
2024-07-31 14:19:11 +0600

    Написан код где будет показываться сообщение на странице

---
+++ index1.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/:message', (req, res) => {
+    res.send(req.params.message);
+});
+
+app.listen(port, () => {
+    console.log('We are live on ' + 'http://127.0.0.1:'+port);
+});
